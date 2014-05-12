8966213ca6a0c0422799e301caccd9f04c26bd78
Christopher Valles <christopher.valles@hailocab.com>
2014-05-12 18:12:46 +0100

    DAT-825 delete queries!

--- redash/models.py
+++ redash/models.py
@@ -274,6 +274,7 @@ class Query(BaseModel):
     ttl = peewee.IntegerField()
     user_email = peewee.CharField(max_length=360, null=True)
     user = peewee.ForeignKeyField(User)
+    is_archived = peewee.BooleanField(default=False, index=True)
     created_at = peewee.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
@@ -295,6 +296,7 @@ class Query(BaseModel):
             'query_hash': self.query_hash,
             'ttl': self.ttl,
             'api_key': self.api_key,
+            'is_archived': self.is_archived,
             'created_at': self.created_at,
             'data_source_id': self._data.get('data_source', None)
         }
@@ -390,6 +392,7 @@ class Dashboard(BaseModel):
         if with_widgets:
             widgets = Widget.select(Widget, Visualization, Query, User)\
                 .where(Widget.dashboard == self.id)\
+                .where(Query.is_archived == False)\
                 .join(Visualization, join_type=peewee.JOIN_LEFT_OUTER)\
                 .join(Query, join_type=peewee.JOIN_LEFT_OUTER)\
                 .join(User, join_type=peewee.JOIN_LEFT_OUTER)
@@ -501,7 +504,7 @@ class Widget(BaseModel):
             d['visualization'] = self.visualization.to_dict()
 
         return d
-
+    
     def __unicode__(self):
         return u"%s" % self.id
 
